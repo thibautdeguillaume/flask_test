0d2273ddd9ad3c36301cd0d50df383b5e27e1295
thibautdeguillaume <thibautdeguillaume@gmail.com>
2020-05-14 18:21:11 +0200

    suppression des fichiers

--- README.md
+++
@@ -1,2 +0,0 @@
-# falsk_test
-test
--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
-#    return 'OK!'
+    return 'OK!'
 
 if __name__ == "__main__":
     app.run()
